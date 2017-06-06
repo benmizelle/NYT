b2350df5f9efd96437b3f864906031a9a833f32e
Ben Mizelle <Ben Mizelle>
2017-06-05 21:19:04 -0400

    diving into REACT

---
+++ app/app.js
@@ -0,0 +1,11 @@
+// ============================================================================
+// Main React Dependencies ====================================================
+// ============================================================================
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the main Main Component
+var Main = require("./components/Main");
+
+// Renders main component (in this case Main)
+ReactDOM.render(<Main />, document.getElementById("app"));
